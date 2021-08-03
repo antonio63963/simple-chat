711078b39eef88087a8cd8638bf3375a33daa9e7
unknown <shotvideo25@gmail.com>
2021-08-03 18:11:49 +0300

    add start animation

--- public/front.js
+++ public/front.js
@@ -1,10 +1,14 @@
 
 console.log('works');
-const socket = io();
+// const socket = io();
 const inputMsg = document.querySelector('#userMsg');
 const msgBtn = document.querySelector('.sendMsgBtn');
 const allMessages = document.querySelector('.messages');
 const inputName = document.querySelector('.userName');
+const submitName = document.querySelector('.submitName');
+const submitForm = document.querySelector('.submitForm');
+const container = document.querySelector('.container');
+
 let userName = '';
 
 
@@ -24,15 +28,14 @@ function showWriting(data) {
   allMessages.insertAdjacentHTML("beforeend", writer);
 }
 function addMessage(data, typeMes = '') {
-  console.log(data);
   const msg = data.msg;
   const name = data.userName;
-    const newMess = `
+  const newMess = `
     <div class="newMsg ${typeMes}">
-    <div class="circleName">
-      <h6>${name}</h6>
-    </div>
-    <p>${msg}</p>
+      <div class="circle partice">
+        <h6>${name}</h6>
+      </div>
+      <p>${msg}</p>
     </div>
   `;
   allMessages.insertAdjacentHTML("beforeend", newMess);
@@ -40,25 +43,34 @@ function addMessage(data, typeMes = '') {
 
 //send- recieve new msg
 
-msgBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log(inputMsg);
-  const msg = inputMsg.value;
-  socket.emit('/chat', {msg, userName});
-  addMessage({msg, userName}, 'mySms');
-  inputMsg.value = '';
+// msgBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   const msg = inputMsg.value;
+//   socket.emit('/chat', {msg, userName});
+//   addMessage({msg, userName}, 'mySms');
+//   inputMsg.value = '';
   
-});
+// });
 
-socket.on('/newMsg', data => {
-  console.log(data);
-  addMessage(data, 'notMySms');
-});
+// socket.on('/newMsg', data => {
+//   console.log(data);
+//   addMessage(data, 'notMySms');
+// });
+
+submitForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.remove('smallSize');
+  document.querySelector('header').classList.remove('startHeader');
+  document.querySelector('.participants').classList.remove('startParticipants');
+})
+
 
 // whatchig for input changes
-inputName.addEventListener('change', (e) => {
-  userName = e.target.value;
-});
+// submitName.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   userName = inputName.value;
+//   socket.emit('sendName', userName);
+// });
 // inputMsg.addEventListener('change', (e) => {
 //   socket.emit('mesHasWrote', {userName});
 // });
@@ -66,15 +78,15 @@ inputName.addEventListener('change', (e) => {
 // inputMsg.addEventListener('input', (e) => {
 //   socket.emit('mesWriting', {userName});
 // });
-socket.on('whoHasWrote', data => {
-  delElem(data.id)
-});
+// socket.on('whoHasWrote', data => {
+//   delElem(data.id)
+// });
 
-socket.on('whoIsWriting', (data) => {
-  if(!document.getElementById(`${data.id}`)) {
-    showWriting(data);
-  } 
-});
+// socket.on('whoIsWriting', (data) => {
+//   if(!document.getElementById(`${data.id}`)) {
+//     showWriting(data);
+//   } 
+// });
 
 
 
